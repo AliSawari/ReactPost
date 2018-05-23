4f25a4c78b509f1d976a1796dd9a820f437c0950
mrghost <ali.dragon.swr@gmail.com>
2018-05-23 20:50:53 +0430

    adding readme & some changes

---
+++ README.md
@@ -0,0 +1,22 @@
+# ReactPost [![dependencies Status](https://david-dm.org/AliSawari/ReactPost/status.svg)](https://david-dm.org/AliSawari/ReactPost)
+**Another simple React Todo App :notebook: :electron:**
+
+_this time with more functionality_ :smiley:
+
+Conside this as VOL2 of My [ReactTodo](https://github.com/AliSawari/ReactTodo)
+
+# Install
+`yarn` or `npm i`
+
+## Run
+`npm start`
+
+- will build a minified build file
+- will start the app on [localhost:3000](http://localhost:3000)
+
+## Dev Run
+**In order to run in Development mode:**
+`yarn dev` or `npm run dev`
+then you can run:
+`node .`
+_tip: Better run `nodemon .` here_ :+1:
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -32,7 +32,7 @@ class PostList extends Component {
 
   render(){
     return <div className="jumbotron">
-      <h1 className="display-3">Search Example</h1>
+      <h1 className="display-3">React Post</h1>
       <p className="lead">This is a simple app shows how Searching works in React-Redux applications</p>
       <hr className="my-4"/>
         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
@@ -48,5 +48,11 @@ class PostList extends Component {
       </div>
   }
 }
+
+// function whatNot(posts){
+//   if(posts.length > 4) {
+//     alert("Enjoying the App? Help us improve by contributing to the project :)");
+//   }
+// }
 
 export default connect(s => s)(PostList);
