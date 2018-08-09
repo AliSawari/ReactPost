c57aabd8eb2b82b2b9146e5b74e33fa9c137776b
hossein <ali.dragon.swr@gmail.com>
2018-08-09 22:25:23 +0430

    space

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
   res.removeHeader('x-powered-by');
   next();
-});
+}); 
 
 app.use(urlencoded({extended:true}));
 app.use(express.static(`${D}/public`));
