2b0bc853d35bebbf3a5bef2c2e9c2647c6898183
hossein <ali.dragon.swr@gmail.com>
2018-10-30 20:53:50 +0330

    updating babel packages

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import Store from './store';
 
 import {Container, PostList} from './components';
 
-
 const Main = () => (
-		<div>
-			<Provider store={Store}>
-				<Container>
-					<PostList />
-				</Container>
-				</Provider>
-		</div>
+	<div>
+		<Provider store={Store}>
+			<Container>
+				<PostList />
+			</Container>
+		</Provider>
+	</div>
 );
 
 
